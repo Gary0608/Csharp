c2a6ac018576df3583da7191110f6e072d6668ad
Gary <yellowgary14@gmail.com>
2025-07-11 16:19:09 +0800

    上傳

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using MyNamespace;
+
+var p1 = new MyNamespace.Person("Gary", "Huang", new DateOnly(2000, 6, 8));
+
+var p2 = new MyNamespace.Person("Mandy", "Huang", new DateOnly(2000, 6, 25));
+
+List<MyNamespace.Person> people = [p1, p2];
+
+p1.Pets.Add(new Cat("Blacky"));
+p1.Pets.Add(new Cat("Siroyi"));
+
+p2.Pets.Add(new Dog("Luffy"));
+
+Console.WriteLine(people.Count);
+
+foreach (var p in people)
+{
+    Console.WriteLine($"{p}");
+
+    foreach (var pet in p.Pets)
+    {
+        Console.WriteLine($"    {pet}");
+    }
+}
+
+namespace MyNamespace
+{
+    public class Person(string firstname, string lastname, DateOnly birthday)
+    {
+        public string First { get; } = firstname;
+        public string Last { get; } = lastname;
+        public DateOnly Birthday { get; } = birthday;
+
+        public List<Pet> Pets { get; } = new();
+
+        public override string ToString()
+        {
+            return $"{First} {Last}";
+        }
+    }
+
+    public abstract class Pet(string firstname)
+    {
+        public string First { get; } = firstname;
+        public abstract string MakeNoise();
+
+        public override string ToString()
+        {
+            return $"{First} and I am {GetType().Name} and I {MakeNoise()}";
+        }
+    }
+
+    public class Cat(string firstname) : Pet(firstname)
+    {
+        public override string MakeNoise() => "meow";
+    }
+
+    public class Dog(string firstname) : Pet(firstname)
+    {
+        public override string MakeNoise() => "bark";
+    }
+}
