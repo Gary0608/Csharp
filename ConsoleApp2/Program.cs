b9b16b6d0503e93cf8c8a536dd1b51aeff2ff274
Gary <yellowgary14@gmail.com>
2025-07-11 14:55:19 +0800

    update

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,16 +1,23 @@
 ﻿List<int> scores = [35, 25, 85, 70, 98, 84, 100, 60, 80];
 
-// IEnumerable<int> scoreQuery =
-//     from score in scores
-//     where score > 80
-//     orderby score descending
-//     select score;
+IEnumerable<int> scoreQuery =
+    from score in scores
+    where score > 80
+    orderby score descending
+    select score;
 
-var scoreQuery = scores.Where(s => s > 80).OrderByDescending(s => s);
+var scoreQuery2 = scores.Where(s => s > 80).OrderByDescending(s => s);
 
 List<int> myScores = scoreQuery.ToList();
 
+List<int> myScores2 = scoreQuery.ToList();
+
 foreach (var score in myScores)
+{
+    Console.WriteLine(score);
+}
+
+foreach (var score in myScores2)
 {
     Console.WriteLine(score);
 }
--- ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8576ada961fa9144e6b096a0b616fbfcb59ea0f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a406d0edc3ea5d0abd14a702def11b86d318373")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
