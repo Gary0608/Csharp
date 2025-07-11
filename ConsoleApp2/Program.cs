8576ada961fa9144e6b096a0b616fbfcb59ea0f8
Gary <yellowgary14@gmail.com>
2025-07-11 14:28:48 +0800

    上傳

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,14 @@
 ﻿List<int> scores = [35, 25, 85, 70, 98, 84, 100, 60, 80];
+
+IEnumerable<int> scoreQuery =
+        from score in scores
+        where score > 80
+        orderby score descending
+        select score;
+
+List<int> myScores = scoreQuery.ToList();
+
+foreach (var score in myScores)
+{
+    Console.WriteLine(score);
+}
